65ce1a742889133159026080b40085988d06359d
jp <jphrmartins@gmail.com>
2023-08-17 19:40:36 -0300

    inicializando repo

---
+++ src/main/java/br/com/amparo/backend/BackendAmparoApplication.java
@@ -0,0 +1,13 @@
+package br.com.amparo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAmparoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAmparoApplication.class, args);
+	}
+
+}
