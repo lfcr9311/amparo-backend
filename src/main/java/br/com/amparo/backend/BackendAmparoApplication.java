3db3b6cbdd010892e3ac4e38634b4e18440cabb6
jp <jphrmartins@gmail.com>
2023-08-23 22:10:38 -0300

    adicionando migration

--- src/main/java/br/com/amparo/backend/BackendAmparoApplication.java
+++ src/main/java/br/com/amparo/backend/BackendAmparoApplication.java
@@ -1,11 +1,13 @@
 package br.com.amparo.backend;
 
+import br.com.amparo.backend.configuration.AmparoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(AmparoConfiguration.class)
 public class BackendAmparoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendAmparoApplication.class, args);
 	}
---
+++ src/main/java/br/com/amparo/backend/configuration/AmparoConfiguration.java
@@ -0,0 +1,7 @@
+package br.com.amparo.backend.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmparoConfiguration {
+}
