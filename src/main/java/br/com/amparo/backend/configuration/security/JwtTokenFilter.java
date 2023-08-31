63d9707a5b50032686ff1ac8f86822edeee11056
jp <jphrmartins@gmail.com>
2023-08-31 20:06:32 -0300

    mudando pra OncePerRequest

--- src/main/java/br/com/amparo/backend/configuration/security/JwtTokenFilter.java
+++ src/main/java/br/com/amparo/backend/configuration/security/JwtTokenFilter.java
@@ -2,35 +2,22 @@ package br.com.amparo.backend.configuration.security;
 
 import br.com.amparo.backend.configuration.security.domain.TokenUser;
 import br.com.amparo.backend.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
 import java.util.Optional;
 
-public class JwtTokenFilter extends AbstractPreAuthenticatedProcessingFilter {
-
-    public JwtTokenFilter() {
-        this.setAuthenticationManager(buildAuthManager(userRepository, tokenService));
-    }
-
+public class JwtTokenFilter extends OncePerRequestFilter {
     @Override
-    protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
-        return Optional.ofNullable(request.getHeader("Authorization"))
-                .map(it -> it.replace("Bearer ", ""))
-                .orElse(null);
-    }
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) {
 
-    @Override
-    protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
-        return "N/A";
-    }
-
-    private AuthenticationManager buildAuthManager(UserRepository userRepository, JwtTokenService tokenService) {
-        return auth -> {
-            String jwtToken = auth.getPrincipal().toString();
-            TokenUser tokenUser = tokenService.decript(jwtToken);
-
-        };
     }
 }
