e6cd838ebb15533124c3f2358222603bef91e466
Luis Felipe Copetti Rossi <rossi.luis@edu.pucrs.br>
2023-09-16 00:57:08 -0300

    fiz: doctor service, doctor service impl, doctor repository, exception e response

--- src/main/java/br/com/amparo/backend/repository/PatientRepository.java
+++ src/main/java/br/com/amparo/backend/repository/PatientRepository.java
@@ -1,7 +1,6 @@
 package br.com.amparo.backend.repository;
 
 import br.com.amparo.backend.domain.entity.Patient;
-import br.com.amparo.backend.exception.PatientCreationException;
 import lombok.extern.java.Log;;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
