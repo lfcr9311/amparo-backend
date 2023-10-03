12f5d95cebde5b301f3fdfb2e7982c8588de9e33
Arthur <pizzolatti.arthur@edu.pucrs.br>
2023-10-02 21:38:40 -0300

    Removido isValid e usando NotNull

--- src/main/java/br/com/amparo/backend/dto/exam/ExamToUpdateRequest.java
+++ src/main/java/br/com/amparo/backend/dto/exam/ExamToUpdateRequest.java
@@ -1,9 +1,8 @@
 package br.com.amparo.backend.dto.exam;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 
-public record ExamToUpdateRequest(String description, LocalDateTime examDate, Boolean isDone, String file, String image) {
-    public boolean isValid() {
-        return this.examDate != null && this.isDone != null;
-    }
+public record ExamToUpdateRequest(String description, @NotNull LocalDateTime examDate,@NotNull Boolean isDone, String file, String image) {
 }
