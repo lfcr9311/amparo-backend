8ad01a42802937de8a97186d7247044195b937d7
Luis Felipe Copetti Rossi <rossi.luis@edu.pucrs.br>
2023-10-07 01:07:23 -0300

    alterando id medicamento para numeric

--- src/main/resources/db/migration/V4__medicine_id.sql
+++ src/main/resources/db/migration/V4__medicine_id.sql
@@ -1,1 +1,7 @@
 ALTER TABLE "Medicine" ALTER COLUMN id TYPE numeric;
+
+ALTER TABLE "Dosage" ALTER COLUMN id_medicine TYPE numeric;
+
+ALTER TABLE "Incompatibility"
+    ALTER COLUMN id_medicine TYPE numeric,
+    ALTER COLUMN id_medicine_inc TYPE numeric;
