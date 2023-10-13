e7ae7bd058eaabdd43f5c8fbfce7f7a0752d0336
Bianca da Silva Alves <biancadsalves@gmail.com>
2023-10-13 16:07:17 -0300

    Alterações na doc de LinkController

--- src/main/java/br/com/amparo/backend/controllers/LinkController.java
+++ src/main/java/br/com/amparo/backend/controllers/LinkController.java
@@ -1,9 +1,11 @@
 package br.com.amparo.backend.controllers;
 
+import br.com.amparo.backend.controllers.dto.ErrorMessage;
 import br.com.amparo.backend.controllers.dto.ObjectMappingError;
 import br.com.amparo.backend.service.LinkService;
 import br.com.amparo.backend.service.security.SecurityUtils;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -33,11 +35,20 @@ public class LinkController {
                     @ApiResponse(responseCode = "500", description = "Linking failed",
                             content = @Content(schema = @Schema(implementation = ObjectMappingError.class))
                     ),
-                    @ApiResponse(responseCode = "409", description = "Link already exists")
+                    @ApiResponse(responseCode = "409", description = "Link already exists",
+                            content = @Content(schema = @Schema(implementation = ErrorMessage.class))
+                    )
             })
     @PreAuthorize("hasRole('DOCTOR')")
     @PostMapping("/{id}")
-    public ResponseEntity<?> linkDoctorToPatient(@PathVariable String id) {
+    public ResponseEntity<?> linkDoctorToPatient(
+            @PathVariable
+            @Parameter(
+                    name = "id",
+                    description = "Patient Id",
+                    example = "a7f6b9c0a8f0d2c4f1e9b5c8f3c6a0e2a3d9b4d1a7d3e6c5a9f8b7d0a8f1e2c4"
+            ) String id
+    ) {
          if (linkService.checkConnection(SecurityUtils.getApiUser().getId(), id)) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
