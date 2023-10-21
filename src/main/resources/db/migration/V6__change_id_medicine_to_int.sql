9ab1d24795f2addff93a2f6ae85a60e108771143
Luis Felipe Copetti Rossi <rossi.luis@edu.pucrs.br>
2023-10-21 03:30:20 -0300

    refatorando id medicines para int e adicionando medicamentos e incompatibilitdades no banco

--- src/main/resources/db/migration/V6__change_id_medicine_to_int.sql
+++ src/main/resources/db/migration/V6__change_id_medicine_to_int.sql
@@ -1,6 +1,6 @@
-ALTER TABLE "Dosage" DROP CONSTRAINT IF EXISTS id_medicine;
-ALTER TABLE "Incompatibility" DROP CONSTRAINT IF EXISTS id_medicine;
-ALTER TABLE "Incompatibility" DROP CONSTRAINT IF EXISTS id_medicine_inc;
+ALTER TABLE "Dosage" DROP CONSTRAINT IF EXISTS "Dosage_id_medicine_fkey";
+ALTER TABLE "Incompatibility" DROP CONSTRAINT IF EXISTS "Incompatibility_id_medicine_fkey";
+ALTER TABLE "Incompatibility" DROP CONSTRAINT IF EXISTS "Incompatibility_id_medicine_inc_fkey";
 
 ALTER TABLE "Medicine" ADD COLUMN new_id SERIAL;
 ALTER TABLE "Incompatibility" ADD COLUMN new_id SERIAL;
@@ -8,15 +8,14 @@ ALTER TABLE "Incompatibility" ADD COLUMN new_id_inc SERIAL;
 ALTER TABLE "Dosage" ADD COLUMN new_id SERIAL;
 
 UPDATE "Medicine" SET new_id = CAST(SUBSTRING(id::text FROM 1 FOR 8) AS INT);
-UPDATE "Incompatibility" SET new_id = CAST(SUBSTRING(id::text FROM 1 FOR 8) AS INT);
-UPDATE "Incompatibility" SET new_id_inc = CAST(SUBSTRING(id::text FROM 1 FOR 8) AS INT);
-UPDATE "Dosage" SET new_id = CAST(SUBSTRING(id::text FROM 1 FOR 8) AS INT);
+UPDATE "Incompatibility" SET new_id = CAST(SUBSTRING(id_medicine::text FROM 1 FOR 8) AS INT);
+UPDATE "Incompatibility" SET new_id_inc = CAST(SUBSTRING(id_medicine_inc::text FROM 1 FOR 8) AS INT);
+UPDATE "Dosage" SET new_id = CAST(SUBSTRING(id_medicine::text FROM 1 FOR 8) AS INT);
 
 ALTER TABLE "Medicine" DROP COLUMN id;
 ALTER TABLE "Incompatibility" DROP COLUMN id_medicine;
 ALTER TABLE "Incompatibility" DROP COLUMN id_medicine_inc;
 ALTER TABLE "Dosage" DROP COLUMN id_medicine;
-
 
 ALTER TABLE "Medicine" RENAME COLUMN new_id TO id;
 ALTER TABLE "Incompatibility" RENAME COLUMN new_id TO id_medicine;
