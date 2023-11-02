487036cf23b5d58728858c8924a868a7d38d911c
Bianca da Silva Alves <biancadsalves@gmail.com>
2023-11-01 23:50:30 -0300

    Remove unused imports

--- src/main/java/br/com/amparo/backend/service/LinkService.java
+++ src/main/java/br/com/amparo/backend/service/LinkService.java
@@ -1,12 +1,8 @@
 package br.com.amparo.backend.service;
 
-import br.com.amparo.backend.domain.entity.Doctor;
 import br.com.amparo.backend.dto.doctor.DoctorResponse;
 import br.com.amparo.backend.dto.patient.PatientResponse;
-import org.springframework.http.ResponseEntity;
-
 import java.util.List;
-import java.util.Optional;
 
 public interface LinkService {
     Boolean linkDoctorToPatient(String doctorId, String patientId);
